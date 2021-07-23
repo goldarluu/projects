44e2cb0e9103e99613406dc07745412ab75f5b9f
Goldar Luu <66859360+goldarluu@users.noreply.github.com>
2021-07-23 04:38:25 -0700

    Added simple Blockchain
    - tutorial by https://davenash.com/2017/10/build-a-blockchain-with-c/ 
    - Understood the basics, but changed a few things about the classes in order for it to be a bit neater in my opinion.

---
+++ Block.h
@@ -0,0 +1,35 @@
+//
+// Created by Dave Nash on 20/10/2017.
+//
+
+#ifndef TESTCHAIN_BLOCK_H
+#define TESTCHAIN_BLOCK_H
+
+#include <cstdint>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+class Block {
+public:
+
+    string sPrevHash;
+
+    string getHash() const { return _sHash;  }
+
+    Block(uint32_t nIndexIn, const string &sDataIn);
+
+    void MineBlock(uint32_t nDifficulty);
+
+private:
+    uint32_t _nIndex;
+    uint32_t _nNonce;
+    string _sData;
+    time_t _tTime;
+    string _sHash; 
+
+    string _CalculateHash() const;
+};
+
+#endif //TESTCHAIN_BLOCK_H
---
+++ Blockchain.cpp
@@ -0,0 +1,20 @@
+
+#include "Blockchain.h"
+
+Blockchain::Blockchain()
+{
+    _vChain.emplace_back(Block(0, "Genesis Block"));
+    _nDifficulty = 6;
+}
+
+void Blockchain::AddBlock(Block bNew)
+{
+    bNew.sPrevHash = _GetLastBlock().getHash();
+    bNew.MineBlock(_nDifficulty);
+    _vChain.push_back(bNew);
+}
+
+Block Blockchain::_GetLastBlock() const
+{
+    return _vChain.back();
+}
